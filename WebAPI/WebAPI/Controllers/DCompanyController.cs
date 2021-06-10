50eb63bb31d5102b0e9a7102c5e9e0c124ce91ae
Juan Felipe Gonzalez Ortiz <felipegonzalezmv@hotmail.com>
2021-06-10 08:51:47 -0500

    Publish in azure and DCompanyController

--- WebAPI/WebAPI/Controllers/DCompanyController.cs
+++ WebAPI/WebAPI/Controllers/DCompanyController.cs
@@ -8,35 +8,35 @@ using WebAPI.Models;
 namespace WebAPI.Controllers
 {
 
+
     [Route("api/[controller]")]
     [ApiController]
-    public class DValidCompaniesController : ControllerBase
+    public class DCompanyController : ControllerBase
     {
- 
         private readonly CompaniesDBContext _context;
-        public DValidCompaniesController(CompaniesDBContext context)
+
+        public DCompanyController(CompaniesDBContext context)
         {
             _context = context;
         }
 
 
         [HttpGet("{id}")]
-
         public async Task<ActionResult> GetValidCompany([FromRoute] int id)
         {
-            var dValidCompany = await _context.DValidCompany.FindAsync(id.ToString());
+            var dCompany = await _context.DCompany.FindAsync(id.ToString());
 
             if (!ModelState.IsValid)
             {
                 return BadRequest();
-            }
+            } 
 
-            if (dValidCompany == null)
+            if (dCompany == null)
             {
                 return NotFound("La identificación de la empresa no está registrada");
             }
 
-            return Ok(dValidCompany);
+            return Ok(dCompany);
         }
 
     }
